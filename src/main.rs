9555fe94938082b0441da9f419941fc6059bab06
nazgull08 <nazgull08@protonmail.com>
2023-02-21 04:18:55 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
 use bevy::prelude::*;
-use bevy_rapier3d::{prelude::{RapierPhysicsPlugin, NoUserData, Collider, RapierContext, QueryFilter, RigidBody, Velocity, ExternalForce}, render::{RapierDebugRenderPlugin, ColliderDebugColor}, rapier::prelude::InteractionGroups};
+use bevy_rapier3d::{prelude::{RapierPhysicsPlugin, NoUserData, Collider, RapierContext, QueryFilter, RigidBody, Velocity, ExternalForce, Damping}, render::{RapierDebugRenderPlugin, ColliderDebugColor}, rapier::prelude::InteractionGroups};
 use voidgrinder::camera::{pan_orbit_camera, spawn_camera};
 
 fn main() {
@@ -12,6 +12,7 @@ fn main() {
         .add_startup_system(setup_light)
         .add_startup_system(setup_physics)
         .add_startup_system(spawn_camera)
+        .init_resource::<Game>()
         .insert_resource(CameraTrackerTimer(Timer::from_seconds(
             1.0,
             TimerMode::Repeating,
@@ -22,6 +23,7 @@ fn main() {
         )))
         .insert_resource(HoveredEntity{value: None,last: None})
         .insert_resource(SelectedEntity{value: None,last: None})
+        .add_system(clean_forces)
         .add_system(pan_orbit_camera)
         .add_system(cast_ray)
         .add_system(camera_tracker_debug)
@@ -46,6 +48,14 @@ struct SelectedEntity{ value: Option<Entity>, last: Option<Entity>}
 #[derive(Component,Debug)]
 struct Selected{selection : bool}
 
+#[derive(Resource, Default)]
+pub struct Game {
+    players: Vec<Option<Entity>>,
+    ground: Option<Entity>,
+    score: i32,
+    cake_eaten: u32,
+}
+
 fn camera_tracker_debug(
     time: Res<Time>,
     mut timer: ResMut<CameraTrackerTimer>,
@@ -61,9 +71,9 @@ fn camera_tracker_debug(
     for (camera, camera_transform) in cameras.iter() {
         let (ray_pos, ray_dir) =
                 ray_from_mouse_position(windows.get_primary().unwrap(), camera, camera_transform);
-        info!("Camera:{:?}",ray_pos);
-        info!("Selection status: {:?}",selected);
-        info!("Hovering status: {:?}",hovered);
+    //    info!("Camera:{:?}",ray_pos);
+    //    info!("Selection status: {:?}",selected);
+    //    info!("Hovering status: {:?}",hovered);
     }
 
 }
@@ -80,9 +90,6 @@ fn selected_entity_tracker(
         selected.value = hovered.value;
         match selected.value {
             Some(ent) => {
-                for (sel) in query.iter_mut() {
-                    info!("{:?}",sel);
-                }
                 let color = Color::GREEN;
                 commands.entity(ent).insert(Selected{selection: true});
                 commands.entity(ent).insert(ColliderDebugColor(color));
@@ -210,7 +217,7 @@ fn setup_light(
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 1500.0,
+            intensity: 5000.0,
             shadows_enabled: true,
             ..default()
         },
@@ -219,55 +226,168 @@ fn setup_light(
     });
 }
 
-pub fn setup_physics(mut commands: Commands) {
+fn clean_forces(
+    time: Res<Time>,
+    mut timer: ResMut<MoveCDTimer>,
+    mut commands: Commands,
+    ext_forces: Query<&ExternalForce>,
+    vel_query: Query<&mut Velocity>,
+    game_state: ResMut<Game>
+) {
+    if !timer.0.tick(time.delta()).finished() {
+        return;
+    }
+    for splayer in &game_state.players{
+       match splayer {
+           None => {},
+           Some(player) => {
+               if let Ok(ext_f) = ext_forces.get(player.to_owned()) {
+                   info!("player: {:?}, force: {:?}",player, ext_f);
+                   commands.entity(player.to_owned()).remove::<ExternalForce>();
+               }
+               if let Ok(vel) = vel_query.get(player.to_owned()) {
+                   info!("player: {:?}, velocity: {:?}",player, vel);
+                   //commands.entity(player.to_owned()).remove::<ExternalForce>();
+               }
+           }
+       }
+   }
+}
+
+pub fn setup_physics(mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut game_state: ResMut<Game>,
+    ) {
+    let mut players = vec![];
+    
+
     /*
      * Ground
      */
     let ground_size = 200.1;
     let ground_height = 0.1;
+    let gmin_x = 0.0 - ground_size;
+    let gmax_x = 0.0 + ground_size;
+    let gmin_z = 0.0 - ground_size;
+    let gmax_z = 0.0 + ground_size;
+    let gmin_y = 0.0 - ground_height;
+    let gmax_y = 0.0 + ground_height;
 
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(0.0, -ground_height, 0.0)),
-        Collider::cuboid(ground_size, ground_height, ground_size),
-    ));
+    let dld = 10.0;
+    let dad = 10.0;
+
+    game_state.ground = Some(commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Box{max_x:gmax_x
+            ,min_x:gmin_x
+            ,min_y:gmin_y
+            ,max_y:gmax_y
+            ,min_z:gmin_z
+            ,max_z:gmax_z})),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    })
+    .insert(Collider::cuboid(ground_size*1.05, ground_height*1.05, ground_size*1.05))
+    .insert(TransformBundle::from(Transform::from_xyz(0.0, -ground_height, 2.0))).id());
 
     let rad = 1.0;
 
+    // load a texture and retrieve its aspect ratio
+    let texture_handle = asset_server.load("res/img/cat.png");
+    let aspect = 0.25;
 
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(0.0, 5.0, 0.0)),
-        RigidBody::Dynamic,
-        Collider::cuboid(rad, rad, rad),
-    )).insert(ExternalForce {
-        force: Vec3::new(0.0, 0.0, 0.0),
-        torque: Vec3::new(0.0, 0.0, 0.0),
-    }).insert(Selected{selection: false});
+// this material renders the texture normally
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        ..default()
+    });
 
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(0.0, 5.0, 3.0)),
-        RigidBody::Dynamic,
-        Collider::cuboid(rad, rad, rad),
-    )).insert(ExternalForce {
+    let mat_purple_glass = materials.add(StandardMaterial {
+        base_color: Color::Rgba { red: 0.4, green: 0.0, blue: 0.8, alpha: 0.9 },
+        alpha_mode: AlphaMode::Blend,
+        metallic: 0.9,
+        ..default()
+    });
+
+    players.push(Some(commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube{ size: rad*2.0})),
+        material: material_handle.clone(),
+        ..default()
+    })
+    .insert(Collider::cuboid(rad, rad, rad))
+    .insert(TransformBundle::from(Transform::from_xyz(0.0, 5.0, 0.0)))
+    .insert(RigidBody::Dynamic)
+    .insert(ExternalForce {
         force: Vec3::new(0.0, 0.0, 0.0),
         torque: Vec3::new(0.0, 0.0, 0.0),
-    }).insert(Selected{selection: false});
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(-5.0, 5.0, 3.0)),
-        RigidBody::Dynamic,
-        Collider::ball(rad),
-    )).insert(ExternalForce {
+    })
+    .insert(Velocity {
+        linvel: Vec3::new(0.0, 0.0, 0.0),
+        angvel: Vec3::new(0., 0.0, 0.0),
+    })
+    .insert(Damping { linear_damping: dld, angular_damping: dad })
+    .insert(Selected{selection: false}).id()));
+
+    players.push(Some(commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube{ size: rad*2.0})),
+        material: mat_purple_glass, 
+        ..default()
+    })
+    .insert(Collider::cuboid(rad, rad, rad))
+    .insert(TransformBundle::from(Transform::from_xyz(-4.0, dld, 0.0)))
+    .insert(RigidBody::Dynamic)
+    .insert(ExternalForce {
         force: Vec3::new(0.0, 0.0, 0.0),
         torque: Vec3::new(0.0, 0.0, 0.0),
-    }).insert(Selected{selection: false});
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(10.0, 5.0, 3.0)),
-        RigidBody::Dynamic,
-        Collider::cylinder(rad, rad),
-    )).insert(ExternalForce {
+    })
+    .insert(Velocity {
+        linvel: Vec3::new(0.0, 0.0, 0.0),
+        angvel: Vec3::new(0., 0.0, 0.0),
+    })
+    .insert(Damping { linear_damping: dld, angular_damping: dad })
+    .insert(Selected{selection: false}).id()));
+
+    players.push(Some(commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere{ radius: rad, subdivisions: 8})),
+        material: material_handle,
+        ..default()
+    })
+    .insert(Collider::ball(rad))
+    .insert(TransformBundle::from(Transform::from_xyz(-dld, dld, 3.0)))
+    .insert(RigidBody::Dynamic)
+    .insert(ExternalForce {
         force: Vec3::new(0.0, 0.0, 0.0),
         torque: Vec3::new(0.0, 0.0, 0.0),
-    }).insert(Selected{selection: false});
-    
+    })
+    .insert(Velocity {
+        linvel: Vec3::new(0.0, 0.0, 0.0),
+        angvel: Vec3::new(0., 0.0, 0.0),
+    })
+    .insert(Damping { linear_damping: dld, angular_damping: dad })
+    .insert(Selected{selection: false}).id()));
+
+    players.push(Some(commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere{ radius: rad, subdivisions: 8})),
+        material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+        ..default()
+    })
+    .insert(Collider::ball(rad))
+    .insert(TransformBundle::from(Transform::from_xyz(10.0, dld, 3.0)))
+    .insert(RigidBody::Dynamic)
+    .insert(ExternalForce {
+        force: Vec3::new(0.0, 0.0, 0.0),
+        torque: Vec3::new(0.0, 0.0, 0.0),
+    })
+    .insert(Velocity {
+        linvel: Vec3::new(0.0, 0.0, 0.0),
+        angvel: Vec3::new(0., 0.0, 0.0),
+    })
+    .insert(Damping{ linear_damping: dld, angular_damping: dad })
+    .insert(Selected{selection: false}).id()));
+
+    game_state.players = players;
 }
 
 
@@ -334,7 +454,8 @@ fn move_selected(
     mut transforms: Query<&mut Transform>,
     time: Res<Time>,
     mut timer: ResMut<MoveCDTimer>,
-    mut query: Query<(&mut ExternalForce, &Selected)>,
+    mut game_state: ResMut<Game>,
+    query: Query<&Selected>
 ) {
         match selected.value {
             Some(ent) => {
@@ -357,12 +478,31 @@ fn move_selected(
                     moved = true;
                 }
         if moved {
-               for (mut f_e,sel) in query.iter_mut() {
+        {
+            for splayer in &game_state.players{
+                match splayer {
+                    None => {},
+                    Some(player) => {
+                        if let Ok(sel) = query.get(player.to_owned()) {
+                            if sel.selection == true {
+                                commands.entity(player.to_owned()).insert(ExternalForce {
+                                    force: Vec3::new(50.0*way.0, 0.0, 50.0*way.1),
+                                    torque: Vec3::new(0.0, 0.0, 0.0),
+
+                                });
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+        /*       for (mut f_e,sel) in query.iter_mut() {
                   if sel.selection {
                    info!("{:?} Force: {:?}",way,f_e);
                     f_e.force = Vec3::new(50.0*way.0, 0.0, 50.0*way.1);
                   } 
-                } 
+                } */
         }
 
 
